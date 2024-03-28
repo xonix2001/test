dd5f48ceae445636126ac343c767fdd2597c2944
hary <haryevsky@gmail.com>
2024-03-28 12:50:29 +0800

    3-28 home

--- test.py
+++ test.py
@@ -130,75 +130,72 @@ if __name__ =="__main__":
 
 
     #性别 姓名 年龄 病历号 影像号 脑电图号 检查号 电话号 主诉 
-    # for n,i in enumerate(list1):
-    #     i=remove_spaces_and_newlines(i)
-        # gender =extractor(r'男|女',i)
-        # name=en(i)
-        # age=ea(i)
-        # case_num=extractor(r"(?:病历号|病例号|病案号|住院号|病案)[：:]?(\d+[\*\d]*)",i)
-        # image_num=extractor(r"(?:影像号|影响号)[：:]?(\d+)",i)
-        # video_num=extractor(r"(?:脑电图号|脑电号)[：:]?(\d+[-\d]*)",i)
-        # check_num=extractor(r"检查号[：:]?(\d+)",i)
-        # phone_num=extractor(r"(?:电话号|电话)[：:]\D*(\d+)",i)
-        #chief=ec(i)
+    for n,i in enumerate(list1):
+        i=remove_spaces_and_newlines(i)
+        gender =extractor(r'男|女',i)
+        name=en(i)
+        age=ea(i)
+        case_num=extractor(r"(?:病历号|病例号|病案号|住院号|病案)[：:]?(\d+[\*\d]*)",i)
+        image_num=extractor(r"(?:影像号|影响号)[：:]?(\d+)",i)
+        video_num=extractor(r"(?:脑电图号|脑电号)[：:]?(\d+[-\d]*)",i)
+        check_num=extractor(r"检查号[：:]?(\d+)",i)
+        phone_num=extractor(r"(?:电话号|电话)[：:]\D*(\d+)",i)
+        chief=ec(i)
         
         
         #添加
-        # list6.append(name)
-        # list7.append(len(name))
-        # list4.append(gender)
-        # list5.append(len(gender))
-        # list8.append(age)
-        # list9.append(len(age))
-        # list10.append(case_num)
-        # list11.append(len(case_num))
-        # list12.append(image_num)
-        # list13.append(len(image_num))
-        # list14.append(video_num)
-        # list15.append(len(video_num))   
-        # list16.append(check_num)
-        # list17.append(len(check_num))
-        # list18.append(phone_num)
-        # list19.append(len(phone_num))   
-        # if chief:
-        #     list20.append(chief)
-        #     list21.append(len(chief))
-        # else:
-        #     cc=remove_spaces_and_newlines(list_cc[n])
-        #     cc=extract_all_complaints(cc)
-        #     list20.append(cc)
-        #     list21.append(len(cc))
-            
-        # list22.append(phone_num)
-        # list23.append(len(phone_num))   
+        list6.append(name)
+        list7.append(len(name))
+        list4.append(gender)
+        list5.append(len(gender))
+        list8.append(age)
+        list9.append(len(age))
+        list10.append(case_num)
+        list11.append(len(case_num))
+        list12.append(image_num)
+        list13.append(len(image_num))
+        list14.append(video_num)
+        list15.append(len(video_num))   
+        list16.append(check_num)
+        list17.append(len(check_num))
+        list18.append(phone_num)
+        list19.append(len(phone_num))   
+        if chief:
+            list20.append(chief)
+            list21.append(len(chief))
+        else:
+            cc=remove_spaces_and_newlines(list_cc[n])
+            cc=extract_all_complaints(cc)
+            list20.append(cc)
+            list21.append(len(cc))
+           
        
     #print(list20)    
     
     #姓名    
-    # for i in list2:
-    #     i=remove_spaces_and_newlines(i)
-    #     name=ep(i)
-    #     list3.append(name)
-    
+    for i in list2:
+        i=remove_spaces_and_newlines(i)
+        name=ep(i)
+        list3.append(name)
     #辅助检查
-    # for n,i in enumerate(list_exam):
-    #     checked=check(n,8) 
-    #     i=checked[0] if checked else None
-    #     location=checked[1] if checked else ''
-    #     i=remove_spaces_and_newlines(i)
-    #     e=i.split('$')
-    #     a=[]
-    #     num=0
-    #     for b in e:
-    #         if b and b!='辅助检查：':
-    #             num+=1
-    #         exam_data=extract_exam_data_refined(b)
-    #         if exam_data:
-    #             for i in exam_data:
-    #                 a.append(i)
-    #     list22.append(a)
-    #     list23.append(num-len(a))
-    #     list24.append(location)
+    for n,i in enumerate(list_exam):
+        checked=check(n,8) 
+        i=checked[0] if checked else None
+        location=checked[1] if checked else ''
+        i=remove_spaces_and_newlines(i)
+        e=i.split('$')
+        a=[]
+        num=0
+        for b in e:
+            if b and b!='辅助检查：':
+                num+=1
+            exam_data=extract_exam_data_refined(b)
+            if exam_data:
+                for i in exam_data:
+                    a.append(i)
+        list22.append(a)
+        list23.append(num-len(a))
+        list24.append(location)
         #print(a,num-len(a),location)
         
 
@@ -244,46 +241,46 @@ if __name__ =="__main__":
     #for n,i in range(1,37):
     # excel1.write_list_to_column(11,list2)
     # excel1.write_list_to_column(11,list3)
-    # excel1.write_list_to_column(12,list6)
-    # excel1.write_list_to_column(13,list7)
+    excel1.write_list_to_column(12,list6)
+    excel1.write_list_to_column(13,list7)
 
 
-    # excel1.write_list_to_column(14,list4)
-    # excel1.write_list_to_column(15,list5)
+    excel1.write_list_to_column(14,list4)
+    excel1.write_list_to_column(15,list5)
 
-    # excel1.write_list_to_column(16,list8)
-    # excel1.write_list_to_column(17,list9)
+    excel1.write_list_to_column(16,list8)
+    excel1.write_list_to_column(17,list9)
 
-    # excel1.write_list_to_column(18,list10)
-    # excel1.write_list_to_column(19,list11)
+    excel1.write_list_to_column(18,list10)
+    excel1.write_list_to_column(19,list11)
 
-    # excel1.write_list_to_column(20,list12)
-    # excel1.write_list_to_column(21,list13)
-    # excel1.write_list_to_column(22,list14)
-    # excel1.write_list_to_column(23,list15)
+    excel1.write_list_to_column(20,list12)
+    excel1.write_list_to_column(21,list13)
+    excel1.write_list_to_column(22,list14)
+    excel1.write_list_to_column(23,list15)
 
-    # excel1.write_list_to_column(24,list16)
-    # excel1.write_list_to_column(25,list17)
-    # excel1.write_list_to_column(26,list18)
-    # excel1.write_list_to_column(27,list19)
-    #excel1.write_list_to_column(28,list20)
-    #excel1.write_list_to_column(29,list21)
-    # excel1.write_list_to_column(30,list22)
-    # excel1.write_list_to_column(31,list23)
-    # excel1.write_list_to_column(32,list24)
-    # excel1.write_list_to_column(33,list25)
-    # excel1.write_list_to_column(34,list26)
-    # excel1.write_list_to_column(35,list27)
+    excel1.write_list_to_column(24,list16)
+    excel1.write_list_to_column(25,list17)
+    excel1.write_list_to_column(26,list18)
+    excel1.write_list_to_column(27,list19)
+    excel1.write_list_to_column(28,list20)
+    excel1.write_list_to_column(29,list21)
+    excel1.write_list_to_column(30,list22)
+    excel1.write_list_to_column(31,list23)
+    excel1.write_list_to_column(32,list24)
+    excel1.write_list_to_column(33,list25)
+    excel1.write_list_to_column(34,list26)
+    excel1.write_list_to_column(35,list27)
     
-    # excel1.write_list_to_column(36,list28)
-    # excel1.write_list_to_column(37,list29)
+    excel1.write_list_to_column(36,list28)
+    excel1.write_list_to_column(37,list29)
     
-    # excel1.write_list_to_column(38,list30)
-    # excel1.write_list_to_column(39,list31)
-    # excel1.write_list_to_column(40,list32)
-    # excel1.write_list_to_column(41,list33)
-    # excel1.write_list_to_column(42,list34)
-    # excel1.write_list_to_column(43,list35)
-    # excel1.write_list_to_column(44,list36)
-    # excel1.write_list_to_column(45,list37)
-    # excel1.write_list_to_column(46,list38)
+    excel1.write_list_to_column(38,list30)
+    excel1.write_list_to_column(39,list31)
+    excel1.write_list_to_column(40,list32)
+    excel1.write_list_to_column(41,list33)
+    excel1.write_list_to_column(42,list34)
+    excel1.write_list_to_column(43,list35)
+    excel1.write_list_to_column(44,list36)
+    #excel1.write_list_to_column(45,list37)
+    #excel1.write_list_to_column(46,list38)
---
+++ test_1.0.py
@@ -0,0 +1,129 @@
+import re
+import Office操作脚本 as of
+from extract_name import (
+    extract_names_with_new_rule as en, 
+    extract_first_chinese_phrase as ep, 
+    extract_all_ages_in_order as ea, 
+    extract_reason_or_complaint as ec, 
+    extractor, 
+    extract_all_complaints, 
+    extract_exam_data, 
+    extract_exam_data_refined
+)
+
+def remove_spaces_and_newlines(text):
+    """Remove spaces and newlines from a string using regex."""
+    return re.sub(r'\s+', '', text) if text and isinstance(text, str) else 'None'
+
+def check(n, col, list_total):
+    """Check for data availability and return the value and its location."""
+    for l, i in enumerate(list_total[8 - col:]):
+        if i[n] and isinstance(i[n], str):
+            return i[n], col - l
+    return None, ''
+
+def from_basic():
+    for n, item in enumerate(columns[0]):  # Assuming the last item in columns corresponds to list1 in original script
+        item_cleaned = remove_spaces_and_newlines(item)
+        
+        # Extract and process data
+        gender = extractor(r'男|女', item_cleaned)
+        name = en(item_cleaned)
+        age = ea(item_cleaned)
+        chief=ec(item_cleaned)
+        case_num=extractor(r"(?:病历号|病例号|病案号|住院号|病案)[：:]?(\d+[\*\d]*)",item_cleaned)
+        image_num=extractor(r"(?:影像号|影响号)[：:]?(\d+)",item_cleaned)
+        video_num=extractor(r"(?:脑电图号|脑电号)[：:]?(\d+[-\d]*)",item_cleaned)
+        check_num=extractor(r"检查号[：:]?(\d+)",item_cleaned)
+        phone_num=extractor(r"(?:电话号|电话)[：:]\D*(\d+)",item_cleaned)
+        
+        data_list=[name,gender,age,case_num,image_num,video_num,check_num,phone_num]
+        
+        # Append extracted data to corresponding lists in the dictionary
+        for n,i in enumerate(data_list):
+            lists[f"list{2*n+1}"].append(i)
+            lists[f"list{2*(n + 1)}"].append(len(i))
+        
+        #chief
+        if chief:
+            lists['list17'].append(chief)
+            lists['list18'].append(len(chief))
+        else:
+            cc=remove_spaces_and_newlines(columns[1][n])
+            cc=extract_all_complaints(cc)
+            lists['list17'].append(cc)
+            lists['list18'].append(len(cc))
+    
+def from_filename():
+    #姓名    
+    lists['name_from_file']=[]
+    for i in columns[8]:
+        i=remove_spaces_and_newlines(i)
+        name=ep(i)
+        lists['name_from_file'].append(name)
+
+def from_exam():
+    #辅助检查
+    for n,i in enumerate(columns[7]):
+        checked=check(n,8) 
+        i=checked[0] if checked else None
+        location=checked[1] if checked else ''
+        i=remove_spaces_and_newlines(i)
+        e=i.split('$')
+        a=[]
+        num=0
+        for b in e:
+            if b and b!='辅助检查：':
+                num+=1
+            exam_data=extract_exam_data_refined(b)
+            if exam_data:
+                for i in exam_data:
+                    a.append(i)
+        lists['list19'].append(a)
+        lists['list20'].append(num-len(a))
+        lists['list21'].append(location)
+
+def from_pexam():
+    #查体
+    for n,i in enumerate(columns[6]):
+        checked=check(n,7) 
+        i=checked[0] if checked else None
+        location=checked[1] if checked else ''
+        i=remove_spaces_and_newlines(i)
+        e=i.split('$')
+        a=[]
+        num=0
+        for b in e:
+            if b and all(key not in b for key in ['MRI','EEG','PET','MEG','脑电图','脑磁图','血药浓度','视野检查','CT','ECG','MRS']):
+                num+=1
+            pexam_data=extract_exam_data_refined(b)
+            if pexam_data:
+                for i in pexam_data:
+                    a.append(i)
+        lists['list22'].append(a)
+        lists['list23'].append(num-len(a))
+        lists['list24'].append(location)
+           
+
+if __name__ == "__main__":
+    file_path = r"C:\Users\11\Desktop\database\main\files\分类结果.xlsx"
+    sheet_name = 'Sheet1'
+    excel = of.excel_extract(file_path, sheet_name)
+    excel1 = of.excel_writer(file_path, sheet_name)
+    
+    # Extract columns from Excel and reverse to match original logic
+    columns = [excel.col_byindex(i, None)[1] for i in range(1, 10)]
+    
+    # Initialize a dictionary for managing lists
+    lists = {f"list{i}": [] for i in range(1, 37)}
+    
+    #extract data
+    from_basic()
+    from_filename()
+    from_exam()
+    from_pexam()
+     
+    # Writing data back to Excel from the dictionary
+    excel1.write_list_to_column(11, lists['name_from_file'])
+    for i in range(1, 37):
+        excel1.write_list_to_column(i + 11, lists[f"list{i}"])
